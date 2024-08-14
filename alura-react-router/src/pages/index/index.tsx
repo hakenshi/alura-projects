428d3534c89c524a113de8e5333d01d46a5a06d7
hakenshi <fkdias025@gmail.com>
2024-08-14 17:53:05 -0300

    new file:   3500-meteora-ecommerce-next-14 	new file:   alura-react-router/.eslintrc.cjs 	new file:   alura-react-router/.gitignore 	new file:   alura-react-router/README.md 	new file:   alura-react-router/bun.lockb 	new file:   alura-react-router/index.html 	new file:   alura-react-router/index.ts 	new file:   alura-react-router/package.json 	new file:   alura-react-router/public/vite.svg 	new file:   alura-react-router/src/App.css 	new file:   alura-react-router/src/App.tsx 	new file:   alura-react-router/src/assets/react.svg 	new file:   alura-react-router/src/index.css 	new file:   alura-react-router/src/main.tsx 	new file:   alura-react-router/src/pages/index/index.tsx 	new file:   alura-react-router/src/vite-env.d.ts 	new file:   alura-react-router/tsconfig.app.json 	new file:   alura-react-router/tsconfig.json 	new file:   alura-react-router/tsconfig.node.json 	new file:   alura-react-router/vite.config.ts

---
+++ alura-react-router/src/pages/index/index.tsx
@@ -0,0 +1,15 @@
+function Index() {
+
+    
+
+    return (
+        <section>
+            <h3>Top 3 frutas fodas</h3>
+            <div>
+
+            </div>
+        </section>
+    );
+}
+
+export default Index;
